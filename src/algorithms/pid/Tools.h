fa4d08af8069ab4781389e48a919daa1637ee6da
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2023-08-21 17:05:30 -0400

    test

--- src/algorithms/pid/Tools.h
+++ src/algorithms/pid/Tools.h
@@ -1,4 +1,4 @@
-// Copyright 2023, Alexander Kiselev, Christopher Dilks
+test// Copyright 2023, Alexander Kiselev, Christopher Dilks
 // Subject to the terms in the LICENSE file found in the top-level directory.
 //
 // Common functions for PID algorithms
